30a23f669d2e2643b728ce75cff2349d6017f48c
EnzoClares <enzoclares1712@gmail.com>
2024-05-24 12:33:19 -0300

    tipos de dados

---
+++ tipos-de-dados/script.js
@@ -0,0 +1,19 @@
+const texto = 'JavaScript';
+const frase = 'esse é meu "jogo"'
+
+console.log(texto + ' ' + frase)
+
+
+let total = 0;
+
+const compras = 30
+const impostos = 10
+
+total = compras + impostos
+
+const string = '20' + '20'
+const numbers  = 20 + 20
+const transform = Number('20') + 20 /* String em número*/
+
+console.log(transform)
+console.log(total)
