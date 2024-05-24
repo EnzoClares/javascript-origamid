30515ed23f7e51f4d7871c76076bf6bb2dd03601
EnzoClares <enzoclares1712@gmail.com>
2024-05-24 12:52:21 -0300

    objeto

---
+++ objeto/script.js
@@ -0,0 +1,26 @@
+const livronome = 'senhor dos aneis'
+const ano = 1954
+const autor = 'j. k. k. tyler'
+
+const filmenome = 'senhor dos aneis'
+const filmeano = 2001
+const diretor = 'peter jackson'
+
+const livro = {
+  nome: 'senhor dos aneis',
+  ano: 1954,
+  autor: 'j. k. k. tyler'
+}
+
+const filme = {
+   nome: 'senhor dos aneis',
+   ano: 2001,
+   autor: 'j. k. k. tyler',
+   diretor: 'peter jackson'
+}
+
+console.log(2022 - livro.ano)
+
+/* usa-se do objeto para evitar criar-se diversas variáveis
+  tudo é objeto
+*/
